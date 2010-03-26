b05ae938a80e3b81363474397d11356843121e50
Jeremy <jeremyfoo@gmail.com>
2010-03-26 15:43:00 +0800

    initial commit of Engine

---
+++ JONTUBusEngine.h
@@ -0,0 +1,18 @@
+//
+//  JONTUBusEngine.h
+//  NTUBusArrival
+//
+//  Created by Jeremy Foo on 3/26/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface JONTUBusEngine : NSObject {
+	NSArray *_buses;
+	NSArray *_routes;
+	NSArray *_busstops;
+}
+
+@end
---
+++ JONTUBusEngine.m
@@ -0,0 +1,14 @@
+//
+//  JONTUBusEngine.m
+//  NTUBusArrival
+//
+//  Created by Jeremy Foo on 3/26/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "JONTUBusEngine.h"
+
+
+@implementation JONTUBusEngine
+
+@end
