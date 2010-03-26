1ec4c9eed35e0d4114ac783cba649f1c84f8feca
Jeremy <jeremyfoo@gmail.com>
2010-03-26 16:27:13 +0800

    + Added Synthesize Singleton Macro. + Added sendXHR method and constant URLs

--- JONTUBusEngine.h
+++ JONTUBusEngine.h
@@ -6,13 +6,16 @@
 //  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
+#import <Foundation/Foundation.h>
+#import "SynthesizeSingleton.h"
 
 @interface JONTUBusEngine : NSObject {
 	NSArray *_buses;
 	NSArray *_routes;
 	NSArray *_busstops;
 }
+
++(JONTUBusEngine *)sharedJONTUBusEngine;
+-(NSData *)sendXHRToURL:(NSString *)url PostValues:(NSDictionary *)postValues;
 
 @end
--- JONTUBusEngine.m
+++ JONTUBusEngine.m
@@ -11,4 +11,40 @@
 
 @implementation JONTUBusEngine
 
+static const NSString *getRouteURL = @"http://campusbus.ntu.edu.sg/ntubus/index.php/main/getCurrentBusStop";
+static const NSString *getBusPosition = @"http://campusbus.ntu.edu.sg/ntubus/index.php/main/getCurrentPosition";
+static const NSString *getEta = @"http://campusbus.ntu.edu.sg/ntubus/index.php/xml/getEta";
+
+SYNTHESIZE_SINGLETON_FOR_CLASS(JONTUBusEngine);
+
+-(NSData *) sendXHRToURL:(NSString *)url PostValues:(NSDictionary *)postValues {
+	
+	NSMutableString *post = [NSMutableString string];
+	for (NSString *key in postValues) {
+		if ([post length] > 0) {
+			[post appendString:@"&"];
+		}
+		[post appendFormat:@"%@=%@",key,[postValues objectForKey:key]];
+	}
+	NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+	NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
+	
+	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+	
+	[request setURL:[NSURL URLWithString:url]];
+	[request setHTTPMethod:@"POST"];
+	[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+	[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+	[request setHTTPBody:postData];
+	
+	NSData *recvData = [[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil] autorelease];
+	
+	[post release];
+	[postData release];
+	[postLength release];
+	[request release];
+	
+	return recvData;
+}
+
 @end
