e80dfe8313df302f0b7c23063fb2bdc795483b9e
Jeremy <jeremyfoo@gmail.com>
2010-04-25 22:10:08 +0800

    test examples for archiving action

--- NTUBusArrival.m
+++ NTUBusArrival.m
@@ -4,11 +4,12 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
+//	[JONTUBusEngine loadState:@"/Users/jeremy/test.dat"];
 	
 	JONTUBusEngine *engine = [JONTUBusEngine sharedJONTUBusEngine];
-
-	[engine setHoldCache:-1];
-	[engine start];
+	
+	[engine setHoldCache:-1];	// comment along with loadState to test archived data
+	[engine start];				// comment along with loadState to test archived data
 
 	for (int i=0;i<[[engine stops] count];i++) {
 		
@@ -18,6 +19,12 @@ int main (int argc, const char * argv[]) {
 	NSLog(@"%i", [[engine stops] count]);
 	
 	NSLog(@"%@",[[[engine stops] objectAtIndex:10] desc]);
+	
+	NSLog(@"%@", [[[engine stops] objectAtIndex:10] arrivals]);
+	
+	
+//	[JONTUBusEngine saveState:@"/Users/jeremy/test.dat"];
+	
 	
 	[pool drain];
     return 0;
